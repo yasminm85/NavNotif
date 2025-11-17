5df86f1dc1f2aed55b3029d064d05c373eae47b6
Yasmin <yasminmutiarabintang@gmail.com>
2025-11-17 16:36:36 +0700

    Login Updated

---
+++ src/views/pages/auth-forms/AuthLogin.jsx
@@ -0,0 +1,88 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+// material-ui
+import { useTheme } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Grid from '@mui/material/Grid2';
+import IconButton from '@mui/material/IconButton';
+import InputAdornment from '@mui/material/InputAdornment';
+import InputLabel from '@mui/material/InputLabel';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+// project imports
+import AnimateButton from 'ui-component/extended/AnimateButton';
+
+// assets
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
+// ===============================|| JWT - LOGIN ||=============================== //
+
+export default function AuthLogin() {
+  const theme = useTheme();
+
+  const [checked, setChecked] = useState(true);
+
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <>
+      <FormControl fullWidth sx={{ ...theme.typography.customInput }}>
+        <InputLabel htmlFor="outlined-adornment-email-login">Email</InputLabel>
+        <OutlinedInput id="outlined-adornment-email-login" type="email" name="email" inputProps={{}} />
+      </FormControl>
+
+      <FormControl fullWidth sx={{ ...theme.typography.customInput }}>
+        <InputLabel htmlFor="outlined-adornment-password-login">Password</InputLabel>
+        <OutlinedInput
+          id="outlined-adornment-password-login"
+          type={showPassword ? 'text' : 'password'}
+          
+          name="password"
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                edge="end"
+                size="large"
+              >
+                {showPassword ? <Visibility /> : <VisibilityOff />}
+              </IconButton>
+            </InputAdornment>
+          }
+          inputProps={{}}
+          label="Password"
+        />
+      </FormControl>
+
+      <Grid container sx={{ alignItems: 'center', justifyContent: 'space-between' }}>
+        <Grid>
+        </Grid>
+        <Grid>
+        </Grid>
+      </Grid>
+      <Box sx={{ mt: 2 }}>
+        <AnimateButton>
+          <Button color="primary" fullWidth size="large" type="submit" variant="contained">
+            Login
+          </Button>
+        </AnimateButton>
+      </Box>
+    </>
+  );
+}
