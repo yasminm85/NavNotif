89272d4f584bf06ace8b4fa6d4148ef879ba212b
Yasmin <yasminmutiarabintang@gmail.com>
2025-11-18 11:09:33 +0700

    updated frontend v2

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -8,9 +8,6 @@ import Loadable from 'ui-component/Loadable';
 const DashboardDefault = Loadable(lazy(() => import('views/dashboard/Default')));
 
 // utilities routing
-const UtilsTypography = Loadable(lazy(() => import('views/utilities/Typography')));
-const UtilsColor = Loadable(lazy(() => import('views/utilities/Color')));
-const UtilsShadow = Loadable(lazy(() => import('views/utilities/Shadow')));
 
 // sample page routing
 const SamplePage = Loadable(lazy(() => import('views/sample-page')));
@@ -33,18 +30,6 @@ const MainRoutes = {
           element: <DashboardDefault />
         }
       ]
-    },
-    {
-      path: 'typography',
-      element: <UtilsTypography />
-    },
-    {
-      path: 'color',
-      element: <UtilsColor />
-    },
-    {
-      path: 'shadow',
-      element: <UtilsShadow />
     },
     {
       path: '/sample-page',
