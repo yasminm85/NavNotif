c930313a934268e0fcf9ead0ea4251df2fa99945
Yasmin <yasminmutiarabintang@gmail.com>
2026-02-04 15:40:29 +0700

    adding comment

--- backend/routes/tindaklanjutRoutes.js
+++ backend/routes/tindaklanjutRoutes.js
@@ -6,10 +6,19 @@ const upload_file_arahan = require('../middleware/uploadFileArahan');
 const upload_file_tindaklanjut = require('../middleware/uploadPatchTindakLanjut')
 const { getTindakLanjut, createTindakLanjut, updateTindakLanjut, getMyArahan, getFile } = require('../controllers/tindaklanjutController')
 
+// router get tindak lanjut
 router.get('/get-tindaklanjut', getTindakLanjut);
+
+// router get arahan sesuai pegawai
 router.get('/get-arahan', verifyToken, authorizationRoles('pegawai', 'admin'), getMyArahan);
+
+// router post untuk membuat arahan dan tindak lanjut
 router.post('/create-tindaklanjut', upload_file_arahan, createTindakLanjut);
+
+// router patch untuk mengisi tindak lanjut
 router.patch('/update/:id/tindaklanjut', verifyToken, authorizationRoles('pegawai', 'admin'),upload_file_tindaklanjut, updateTindakLanjut);
+
+// router get membuka file
 router.get('/file_tindak/:id', getFile);
 
 
